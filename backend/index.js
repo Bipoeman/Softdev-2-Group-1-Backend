06901a12590428bc08ec42f31c159e83577b13cf
Nutpapop Yasawut <66255462+winth03@users.noreply.github.com>
2024-01-14 02:45:07 +0700

    Refactor token verification middleware

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,27 @@
 import express from 'express';
 import login from './routes/login.js';
 import register from './routes/register.js';
-import {verifyToken} from "./controllers/token/token.js";
+import { verifyToken } from "./controllers/token/token.js";
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-let  PUBLIC_PATH = ["/login", "/register", "/" ];
+let PUBLIC_PATH = ["/login", "/register", "/"];
 
 
-app.use((req, res, next) => {
-    if (!(PUBLIC_PATH.includes(req.path))) {
-        console.log(req.headers.authorization);
-        const result = verifyToken(req.headers.authorization);
-        if (result === true) {
-            next();
-        }
-    } else {
-        next();
-    }
-});
+// app.use((req, res, next) => {
+//     if (!(PUBLIC_PATH.includes(req.path))) {
+//         console.log("Header: ", req.headers.authorization);
+//         const result = verifyToken(req.headers.authorization);
+//         if (result === true) {
+//             next();
+//         }
+//     } else {
+//         next();
+//     }
+// });
 
 
 app.use("/login", login);
