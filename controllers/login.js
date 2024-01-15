08ea00dcd5f07e6ce47c0b9220a7743badc5453e
I2eNamE <tapanawat2004@gmail.com>
2024-01-15 13:17:51 +0700

    use token

--- controllers/login.js
+++ controllers/login.js
@@ -1,11 +1,11 @@
-// import { signToken } from "./token/token";
+import { signToken } from "./token/token.js";
 
 
 export const loginController = (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     if (username === "admin" && password === "admin") {
-        res.send("login success");
+        res.send(signToken(2, "admin"));
     }
     else {
         res.status(401).send("login fail");
