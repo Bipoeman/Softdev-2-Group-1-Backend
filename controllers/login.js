7bff0c94a3b30cda935fcce22780a36c3ec32af4
I2eNamE <tapanawat2004@gmail.com>
2024-01-29 13:28:23 +0700

    change register and login from dummy to connect  database system

--- controllers/login.js
+++ controllers/login.js
@@ -1,13 +1,28 @@
 import { signToken } from "./token/token.js";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import conn from "./database/database.js";
+dotenv.config()
+
+const salt = process.env.salt;
 
 
 export const loginController = (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username === "admin" && password === "admin") {
-        res.send(signToken(2, "admin"));
-    }
-    else {
-        res.status(401).send("login fail");
-    }
+    const {email,password} = req.body;
+    const command = "select * from user_info where email = ?";
+    conn.query(command,[email],async (err, result) => {
+        if (err) throw err;
+        else if (result.length !== 1) {
+            return res.status(404).json({error: true, message: 'Email or password is incorrect'});
+        } else {
+            const user = result[0]
+            const passwordValid = await bcrypt.compare(password,user.password );
+            if (!passwordValid) {
+                return res.status(401).json({ error: true, message: 'Email or password is incorrect' });
+            }
+            res.status(200).send(signToken(user.id,user.name))
+
+        }
+    })
+
 }
--- controllers/register.js
+++ controllers/register.js
@@ -1,13 +1,33 @@
-export const registerController = (req, res) => {
+import conn from "./database/database.js";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+dotenv.config()
 
-    const username = req.body.username;
-    const password = req.body.password;
-    const passwordagain = req.body.passwordagain;
-    if (username === "admin" && password === "admin" && passwordagain === "admin") {
-        res.send("register success");
-    }
-    else {
-        res.status(401).send("register fail");
-    }
+const salt = process.env.salt;
+
+
+export const registerController = (req, res) => {
+    const {email,name,password} = req.body;
+    const commandSearch = "select * from user_info where email = ?";
+    const commandAdd = "insert into user_info(email, name, password) VALUES (?,?,?)";
+    conn.query(commandSearch,[email],async (err, result) => {
+        if (err) throw err;
+        else if (result.length !== 0) {
+            res.status(400).send({
+                error: true, message: "this email has been registered"
+            });
+        } else {
+            const hashedPassword = await bcrypt.hash(password,salt )
+            conn.query(commandAdd, [email, name, hashedPassword],(err,result)=>{
+                if (err) throw err;
+                else {
+                    res.status(201).send({
+                        message: "create new user complete",
+                        response: result
+                    })
+                }
+            })
+        }
+    })
 }
 
