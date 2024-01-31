482a9491f25544a1b160f4fb01f2d0414ecab11b
I2eNamE <tapanawat2004@gmail.com>
2024-01-31 10:57:33 +0700

    reformat code in login.js

--- controllers/login.js
+++ controllers/login.js
@@ -1,24 +1,23 @@
-import { signToken } from "./token/token.js";
+import {signToken} from "./token/token.js";
 import bcrypt from "bcryptjs";
 
 import conn from "./database/database.js";
 
 
-
 export const loginController = (req, res) => {
-    const { emailoruser,password} = req.body;
+    const {emailoruser, password} = req.body;
     const command = "select * from user_info where email = ? or username = ?;";
-    conn.query(command,[emailoruser,emailoruser],async (err, result) => {
+    conn.query(command, [emailoruser, emailoruser], async (err, result) => {
         if (err) throw err;
         else if (result.length !== 1) {
             return res.status(404).json({error: true, message: 'Email or password is incorrect'});
         } else {
             const user = result[0]
-            const passwordValid = await bcrypt.compare(password,user.password );
+            const passwordValid = await bcrypt.compare(password, user.password);
             if (!passwordValid) {
-                return res.status(401).json({ error: true, message: 'Email or password is incorrect' });
+                return res.status(401).json({error: true, message: 'Email or password is incorrect'});
             }
-            res.status(200).send(signToken(user.id,user.username))
+            res.status(200).send(signToken(user.id, user.username))
 
         }
     })
