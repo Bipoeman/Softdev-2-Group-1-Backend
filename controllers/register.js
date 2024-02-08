50436a899f9a1cb3d56046780f398470ce5dafd9
I2eNamE <tapanawat2004@gmail.com>
2024-02-08 15:10:13 +0700

    change register login user
    change database system form mysql to supabase and change code in login and register

--- controllers/register.js
+++ controllers/register.js
@@ -1,33 +1,27 @@
-import conn from "./database/database.js";
+import supabase from "./database/database.js";
 import bcrypt from "bcryptjs";
-import dotenv from "dotenv";
-dotenv.config()
 
-const salt = process.env.salt;
+export const registerController = async (req, res) => {
+    const {email, fullname, username, password} = req.body;
 
-
-export const registerController = (req, res) => {
-    const {email,fullname,username,password} = req.body;
-    const commandSearch = "select * from user_info where email = ? or username = ?";
-    const commandAdd = "insert into user_info(email,fullname,username, password) VALUES (?,?,?,?)";
-    conn.query(commandSearch,[email,username],async (err, result) => {
-        if (err) throw err;
-        else if (result.length !== 0) {
-            res.status(400).send({
-                error: true, message: "this email has been registered"
-            });
-        } else {
-            const hashedPassword = await bcrypt.hash(password,10 )
-            conn.query(commandAdd, [email,fullname, username, hashedPassword],(err,result)=>{
-                if (err) throw err;
-                else {
-                    res.status(201).send({
-                        message: "create new user complete",
-                        response: result
-                    })
-                }
-            })
+    const {data, err} = await supabase.from("user_info").select("*").or(`email.eq.${email},username.eq.${username}`);
+    if (err) throw err;
+    else {
+        if(data.length === 0){
+            const hashedPassword = bcrypt.hashSync(password, 10);
+            const {data, error} = await supabase
+                .from('user_info')
+                .insert([
+                    { email, fullname, username, password:hashedPassword }
+                ]);
+            if (error) throw error;
+            else {
+                res.send("it worked");
+            }
         }
-    })
+        else{
+            res.status(400).json({error: true, message: "user already exists"});
+        }
+    }
 }
 
