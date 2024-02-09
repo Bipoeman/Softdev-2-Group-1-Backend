d678a71a0168c5e8dc2b24a5698df4345c6e07ff
I2eNamE <tapanawat2004@gmail.com>
2024-02-09 10:24:35 +0700

    change name variable in login function

--- controllers/login.js
+++ controllers/login.js
@@ -4,11 +4,11 @@ import supabase from "./database/database.js";
 
 
 export const loginController = async (req, res) => {
-    const { emailorusername, password } = req.body;
+    const { emailoruser, password } = req.body;
     const { data, error } = await supabase
         .from('user_info')
         .select('*')
-        .or(`email.eq.${emailorusername},username.eq.${emailorusername}`);
+        .or(`email.eq.${emailoruser},username.eq.${emailoruser}`);
     if (error) throw error;
     else {
         if (data.length === 0) {
@@ -18,7 +18,7 @@ export const loginController = async (req, res) => {
             const user = data[0];
             const validPassword = await bcrypt.compare(password, user.password);
             if (validPassword) {
-                res.status(200).send(signToken(user.id, user.email));
+                res.status(200).send(signToken(user.id, user.role));
             }
             else {
                 res.status(400).json({ error: true, message: "invalid password" });
