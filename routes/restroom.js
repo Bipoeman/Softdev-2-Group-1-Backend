4f8a1e12fffe4c1c17efa207150e37cecd03200c
I2eNamE <tapanawat2004@gmail.com>
2024-02-16 14:12:15 +0700

    start 101 restroom

--- routes/restroom.js
+++ routes/restroom.js
@@ -3,4 +3,21 @@ import express from "express";
 const router = express.Router();
 
 
+// get data of toliet
+router.get("/", (req, res) => {});
+
+// get data of toliet by admin role ??
+router.get("/admin", (req, res) => {});
+
+
+router.post
+
+
+
+// create comment for toliet
+router.post("/comment", (req, res) => {
+});
+
+
+
 export default router;
