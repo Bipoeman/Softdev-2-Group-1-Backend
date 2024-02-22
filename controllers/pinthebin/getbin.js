6e60aed2b5f7685207f54fb735cacf66065bf2bb
Nuttawon <s6501012630107@email.kmutnb.ac.th>
2024-02-22 14:19:40 +0700

    fix {}

--- controllers/pinthebin/getbin.js
+++ controllers/pinthebin/getbin.js
@@ -1,5 +1,5 @@
 import supabase from "../database/database.js";
-import {decodeToken} from "../token/token.js";
+import { decodeToken } from "../token/token.js";
 
 export const getbin = async (req, res) => {
   const { data, error } = await supabase.from("bin_info").select("*");
@@ -22,19 +22,23 @@ export const getbinbyid = async (req, res) => {
     } else {
       res.send(data);
     }
-}
+  }
+};
 
-
-export const getbinbyuserid = async (req,res)=>{
-    const userId = decodeToken(req.headers.authorization).userId;
-    const {data,error} = await supabase.from("bin_info").select("*").eq("user_update",userId);
-    if (error) throw error;
-    else{
-        if (data.length === 0){res.status(404).send("bin not found")}
-        else{
-            res.send(data)
-        }
+export const getbinbyuserid = async (req, res) => {
+  const userId = decodeToken(req.headers.authorization).userId;
+  const { data, error } = await supabase
+    .from("bin_info")
+    .select("*")
+    .eq("user_update", userId);
+  if (error) throw error;
+  else {
+    if (data.length === 0) {
+      res.status(404).send("bin not found");
+    } else {
+      res.send(data);
     }
+  }
 };
 
 export const searchbin = async (req, res) => {
