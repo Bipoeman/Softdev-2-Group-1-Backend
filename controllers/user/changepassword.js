7fea412f4ac820ec513640bdd0ce1c9b8a382ef9
I2eNamE <tapanawat2004@gmail.com>
2024-02-23 16:16:25 +0700

    delete select schema

--- controllers/user/changepassword.js
+++ controllers/user/changepassword.js
@@ -6,7 +6,7 @@ export const changepassword = async (req,res)=>{
     const id = decodeToken(req.headers.authorization).userId;
     const {newpassword} = req.body;
     const hashedPassword = bcrypt.hashSync(newpassword, 10);
-    const {data,err} = await supabase.schema("public").from("user_info").update({password: hashedPassword}).eq("id",id);
+    const {data,err} = await supabase.from("user_info").update({password: hashedPassword}).eq("id",id);
     if (err) throw err;
     else{
         res.send(data)
