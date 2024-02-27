3f4b939d77d7a126b160e674d02fdb04e60d0363
I2eNamE <tapanawat2004@gmail.com>
2024-02-27 21:00:38 +0700

    delete old refresh token

--- controllers/user/renewtoken.js
+++
@@ -1,6 +0,0 @@
-import {decodeToken, signToken} from "../token/token.js";
-
-export const renewtoken = async (req, res) => {
-    const user = decodeToken(req.headers.authorization);
-    res.send(signToken(user.userId, user.role));
-};
