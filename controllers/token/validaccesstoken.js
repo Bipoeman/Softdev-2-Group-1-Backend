b5b25e2b45c29528209842d191c2aa3c6bbe0887
I2eNamE <tapanawat2004@gmail.com>
2024-02-28 15:37:39 +0700

    create issue path

--- controllers/token/validaccesstoken.js
+++ controllers/token/validaccesstoken.js
@@ -6,7 +6,7 @@ const acesssecretKey = process.env.acesssecretKey
 
 export const validaccesstoken = (req,res,next) => {
     const authHeader = req.headers.authorization
-    if(!authHeader){res.status(401).json({msg : 'Unauthorized: Missing access token'})}
+    if(!authHeader){res.status(403).json({msg : 'Unauthorized: Missing access token'})}
     const token = authHeader.split(' ')[1];
     jwt.verify(token, acesssecretKey, (err, decoded) => {
         if (err) {
--- index.js
+++ index.js
@@ -2,13 +2,12 @@ import express from 'express';
 import login from './routes/login.js';
 import register from './routes/register.js';
 import PinTheBin from "./routes/pinthebin.js"
-import { verifyToken } from "./controllers/token/token.js";
 import { config } from 'dotenv';
 import restroom from "./routes/restroom.js";
 import dekhor from './routes/dekhor.js';
 import cors from 'cors';
 import user from "./routes/user.js";
-import support from "./routes/support.js";
+import issue from "./routes/issue.js";
 config();
 const app = express();
 app.use(cors());
@@ -23,7 +22,7 @@ app.use("/user", user);
 app.use("/pinthebin",PinTheBin);
 app.use("/restroom",restroom);
 app.use("/dekhor",dekhor)
-app.use("/support",support)
+app.use("/issue",issue)
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
