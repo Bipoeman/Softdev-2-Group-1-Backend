e13c92db77271dd680e3ae9ddc6c69fe65c875e1
I2eNamE <tapanawat2004@gmail.com>
2024-02-28 14:26:07 +0700

    restructure get user data

--- routes/user.js
+++ routes/user.js
@@ -1,44 +1,22 @@
 import express from "express";
-import supabase from "../controllers/database/database.js";
-import { decodeToken } from "../controllers/token/token.js";
 import { uploadprofilecontroller } from "../controllers/user/uploadprofile.js";
 import multer from "multer";
 import {changeprofile} from "../controllers/user/changeprofile.js";
 import {changepassword} from "../controllers/user/changepassword.js";
 import {resetpassword} from "../controllers/user/resetpassword.js";
 import {refreshtoken} from "../controllers/token/refreshaccesstoken.js";
+import {getuserdata} from "../controllers/user/getuserdata.js";
+import {getuserdatabyid} from "../controllers/user/getuserdatabyid.js";
 
 
 const uploadprofile = multer();
 const router = express.Router();
 
 // all that is path  url/user
-router.get("", async (req, res) => {
-    const {data, error} = await supabase.from("user_info").select("*");
-    if (error) {res.status(500).send(error)} 
-    else{
-        res.send(data)
-    }
-});
+router.get("", getuserdata);
 
 
-router.get("/id", async (req, res) => {
-    const id = decodeToken(req.headers.authorization).userId;
-    const {data, error} = await supabase.from("user_info")
-        .select("id,email,fullname,username,phonenum,birthday,profile,role,description")
-        .eq("id", id);
-
-    if (error) {res.status(500).send(error)} 
-    else{
-        if(data.length === 0){
-            res.status(404).send("user not found")
-        }   
-        else{
-            // delete id,password
-            res.send(data[0])
-        }
-    }
-});
+router.get("/id", getuserdatabyid);
 
 router.post("/upload",uploadprofile.single("file"),uploadprofilecontroller);
 
