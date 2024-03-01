8bd9df3f9e0e679bae3ff9b222f73a1f448d82ab
Nuttawon <s6501012630107@email.kmutnb.ac.th>
2024-03-02 00:24:25 +0700

    add report and picture of report

--- controllers/pinthebin/addbinreport.js
+++ controllers/pinthebin/addbinreport.js
@@ -1,20 +1,47 @@
 import supabase from "../database/database.js";
-import {decodeToken} from "../token/token.js";
+import { decodeToken } from "../token/token.js";
 
 export const addbinreport = async (req, res) => {
-    const userId = decodeToken(req.headers.authorization).userId;
-    const {description, binId,category,header} = req.body;
-    const {data, error} = await supabase.from("bin_report")
-        .insert([{
-            description,
-            bin_id:binId,
-            category,
-            header,
-            user_report:userId
-        }])
-        .select();
-    if (error) throw error;
-    else{
-        res.send(data);
+  const userId = decodeToken(req.headers.authorization).userId;
+  const { description, binId, header } = req.body;
+  const { data, error } = await supabase
+    .from("bin_report")
+    .insert([
+      {
+        description,
+        bin_id: binId,
+        header,
+        user_report: userId,
+      },
+    ])
+    .select();
+  if (error) throw error;
+  else {
+    res.send(data);
+  }
+};
+
+export const addpictureReportController = async (req, res) => {
+  const file = req.file;
+  const binReport = req.body.id;
+  const newminetype = "image/jpeg";
+  const newfilename = `bin_${binReport}.jpeg`;
+  const { data: datapicture, err } = await supabase.storage
+    .from("bin_report")
+    .upload(newfilename, file.buffer, {
+      contentType: newminetype,
+    });
+  if (err) throw err;
+  else {
+    const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
+    const { data, err } = await supabase
+      .from("bin_report")
+      .update({ picture: url })
+      .eq("id", binReport);
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(data);
     }
+  }
 };
--- routes/pinthebin.js
+++ routes/pinthebin.js
@@ -1,47 +1,60 @@
 import express from "express";
-import {getbin, getbinbyid, getbinbyuserid } from "../controllers/pinthebin/getbin.js";
-import {addbin, addpictureController} from "../controllers/pinthebin/addbin.js";
-import {updatebin} from "../controllers/pinthebin/updatebin.js";
+import {
+  getbin,
+  getbinbyid,
+  getbinbyuserid,
+} from "../controllers/pinthebin/getbin.js";
+import {
+  addbin,
+  addpictureController,
+} from "../controllers/pinthebin/addbin.js";
+import { updatebin } from "../controllers/pinthebin/updatebin.js";
 import multer from "multer";
-import {getbinreport} from "../controllers/pinthebin/getbinreport.js";
-import {deletebin} from "../controllers/pinthebin/deletebin.js";
-import {addbinreport} from "../controllers/pinthebin/addbinreport.js";
-import {findbinlike} from "../controllers/pinthebin/findbinlike.js";
-import {validaccesstoken} from "../controllers/token/validaccesstoken.js";
+import { getbinreport } from "../controllers/pinthebin/getbinreport.js";
+import { deletebin } from "../controllers/pinthebin/deletebin.js";
+import { addbinreport } from "../controllers/pinthebin/addbinreport.js";
+import { findbinlike } from "../controllers/pinthebin/findbinlike.js";
+import { validaccesstoken } from "../controllers/token/validaccesstoken.js";
+import { addpictureReportController } from "../controllers/pinthebin/addbinreport.js";
 
 const uploadpicture = multer();
 const router = express.Router();
 
 router.get("", (req, res) => {
-    res.send("test in pinthebin")
-})
+  res.send("test in pinthebin");
+});
 
-router.get("/bin",getbin)
+router.get("/bin", getbin);
 
-router.get("/find",findbinlike)
+router.get("/find", findbinlike);
 
-router.get("/bin/:id",getbinbyid )
+router.get("/bin/:id", getbinbyid);
 
-router.get("/mybin",validaccesstoken,getbinbyuserid)
+router.get("/mybin", validaccesstoken, getbinbyuserid);
 
 //add bin_info
-router.post("/bin",validaccesstoken, addbin);
+router.post("/bin", validaccesstoken, addbin);
 
 // add picture to bin_info
 
-router.post("/bin/upload",uploadpicture.single("file"),addpictureController);
+router.post("/bin/upload", uploadpicture.single("file"), addpictureController);
 
 //update bin_info
-router.put("/bin",validaccesstoken, updatebin);
+router.put("/bin", validaccesstoken, updatebin);
 
 // delete bin_info
-router.delete("/bin/:id",deletebin);
+router.delete("/bin/:id", deletebin);
 //show bin report
-router.get("/report",getbinreport);
+router.get("/report", getbinreport);
 
-router.post("/report",validaccesstoken,addbinreport); //add report
+router.post("/report", validaccesstoken, addbinreport); //add report
+
+router.post(
+  "/report/upload",
+  uploadpicture.single("file"),
+  addpictureReportController
+); //add picture to report
 
 // last 3 is app report is portal app report
-
 
 export default router;
