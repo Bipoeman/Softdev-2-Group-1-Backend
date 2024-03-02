f8aa158d65109e43c5918523a21ff3931225912d
winth03 <66255462+winth03@users.noreply.github.com>
2024-03-03 00:02:27 +0700

    change upload toilet comment function

--- controllers/restroom/uploadtoiletcomment.js
+++ controllers/restroom/uploadtoiletcomment.js
@@ -2,48 +2,28 @@ import supabase from "../database/database.js";
 
 export const uploadtoiletcomment = async (req, res) => {
   const file = req.file;
-  const { commentid } = req.body;
+  const { id } = req.body;
   const newminetype = "image/jpeg";
-  const newfilename = `toilet_comment_${commentid}.jpeg`;
-  const { data, error } = await supabase
-    .from("toilet_comment")
-    .select("picture")
-    .eq("id", commentid);
+  const newfilename = `toilet_comment_${id}.jpeg`;
+  const { data: datapicture, error } = await supabase.storage
+    .from("restroom_comment")
+    .upload(newfilename, file.buffer, {
+      contentType: newminetype,
+      upsert: true
+    });
   if (error) {
     res.status(500).send(error);
-  } else if (data.length === 0) {
-    res.status(404).send("toilet not found");
-  } else if (data[0].picture === null) {
-    const { data: datapicture, error } = await supabase.storage
-      .from("restroom_comment")
-      .upload(newfilename, file.buffer, {
-        contentType: newminetype,
-      });
-    if (error) {
-      res.status(500).send(error);
-    } else {
-      const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
-      const { data, err } = await supabase
-        .from("toilet_comment")
-        .update({ picture: url })
-        .eq("id", commentid)
-        .select();
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send(data);
-      }
-    }
   } else {
-    const { data, err } = await supabase.storage
-      .from("restroom_comment")
-      .update(newfilename, file.buffer, {
-        contentType: newminetype,
-      });
+    const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
+    const { data, err } = await supabase
+      .from("toilet_comment")
+      .update({ picture: url })
+      .eq("id", id)
+      .select();
     if (err) {
       res.status(500).send(err);
     } else {
       res.send(data);
     }
   }
-};
+}
