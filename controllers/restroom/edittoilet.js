2f4d153c8dc8b3e904db02f071d4c074c9f90448
Nuttawon <s6501012630107@email.kmutnb.ac.th>
2024-03-02 20:55:14 +0700

    add address in body edit toilet

--- controllers/restroom/edittoilet.js
+++ controllers/restroom/edittoilet.js
@@ -1,12 +1,13 @@
 import supabase from "../database/database.js";
 
 export const edittoilet = async (req, res) => {
-  const { name, type, for_who, id } = req.body;
+  const { name, type, for_who, id, address } = req.body;
   const { data, error } = await supabase
     .from("toilet_info")
     .update({
       name,
       type,
+      address,
       for_who: JSON.parse(for_who),
     })
     .eq("id", id);
