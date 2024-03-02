2075c2b8e92cc0ccb1831fd8a181ee5301335133
winth03 <66255462+winth03@users.noreply.github.com>
2024-03-03 02:28:01 +0700

    Revert upload picture functions

--- controllers/restroom/uploadtoiletcomment.js
+++ controllers/restroom/uploadtoiletcomment.js
@@ -2,28 +2,48 @@ import supabase from "../database/database.js";
 
 export const uploadtoiletcomment = async (req, res) => {
   const file = req.file;
-  const { id } = req.body;
+  const { commentid } = req.body;
   const newminetype = "image/jpeg";
-  const newfilename = `toilet_comment_${id}.jpeg`;
-  const { data: datapicture, error } = await supabase.storage
-    .from("restroom_comment")
-    .upload(newfilename, file.buffer, {
-      contentType: newminetype,
-      upsert: true
-    });
+  const newfilename = `toilet_comment_${commentid}.jpeg`;
+  const { data, error } = await supabase
+    .from("toilet_comment")
+    .select("picture")
+    .eq("id", commentid);
   if (error) {
     res.status(500).send(error);
+  } else if (data.length === 0) {
+    res.status(404).send("toilet not found");
+  } else if (data[0].picture === null) {
+    const { data: datapicture, error } = await supabase.storage
+      .from("restroom_comment")
+      .upload(newfilename, file.buffer, {
+        contentType: newminetype,
+      });
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
+      const { data, err } = await supabase
+        .from("toilet_comment")
+        .update({ picture: url })
+        .eq("id", commentid)
+        .select();
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(data);
+      }
+    }
   } else {
-    const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
-    const { data, err } = await supabase
-      .from("toilet_comment")
-      .update({ picture: url })
-      .eq("id", id)
-      .select();
+    const { data, err } = await supabase.storage
+      .from("restroom_comment")
+      .update(newfilename, file.buffer, {
+        contentType: newminetype,
+      });
     if (err) {
       res.status(500).send(err);
     } else {
       res.send(data);
     }
   }
-}
+};
--- controllers/restroom/uploadtoiletpicture.js
+++ controllers/restroom/uploadtoiletpicture.js
@@ -2,25 +2,44 @@ import supabase from "../database/database.js";
 
 export const uploadtoiletpicture = async (req, res) => {
   const file = req.file;
-  const { id } = req.body;
+  const { toiletid } = req.body;
   const newminetype = "image/jpeg";
   const newfilename = `toilet_${id}.jpeg`;
-
-  const { data: datapicture, error } = await supabase.storage
-    .from("restroom")
-    .upload(newfilename, file.buffer, {
-      contentType: newminetype,
-      upsert: true
-    });
+  const { data, error } = await supabase
+    .from("toilet_info")
+    .select("picture")
+    .eq("id", toiletid);
   if (error) {
     res.status(500).send(error);
+  } else if (data.length === 0) {
+    res.status(404).send("toilet not found");
+  } else if (data[0].picture === null) {
+    const { data: datapicture, error } = await supabase.storage
+      .from("restroom")
+      .upload(newfilename, file.buffer, {
+        contentType: newminetype,
+      });
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
+      const { data, err } = await supabase
+        .from("toilet_info")
+        .update({ picture: url })
+        .eq("id", toiletid)
+        .select();
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(data);
+      }
+    }
   } else {
-    const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
-    const { data, err } = await supabase
-      .from("toilet_info")
-      .update({ picture: url })
-      .eq("id", id)
-      .select();
+    const { data, err } = await supabase.storage
+      .from("restroom")
+      .update(newfilename, file.buffer, {
+        contentType: newminetype,
+      });
     if (err) {
       res.status(500).send(err);
     } else {
