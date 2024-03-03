72698c7a8737765c1ba5cfc3143b9089c308f8c6
Nuttawon <s6501012630107@email.kmutnb.ac.th>
2024-03-03 15:41:55 +0700

    add  json.parse

--- controllers/pinthebin/addbin.js
+++ controllers/pinthebin/addbin.js
@@ -1,60 +1,82 @@
 import supabase from "../database/database.js";
-import {decodeToken} from "../token/token.js";
+import { decodeToken } from "../token/token.js";
 
 export const addbin = async (req, res) => {
-    const userId = decodeToken(req.headers.authorization).userId;
-    const {location , description = null , bintype  , latitude ,longitude} = req.body;
-    const {data,error} = await supabase.from("bin_info").select("*").eq("latitude",latitude).eq("longitude",longitude);
+  const userId = decodeToken(req.headers.authorization).userId;
+  const {
+    location,
+    description = null,
+    bintype,
+    latitude,
+    longitude,
+  } = req.body;
+  const { data, error } = await supabase
+    .from("bin_info")
+    .select("*")
+    .eq("latitude", latitude)
+    .eq("longitude", longitude);
+  if (error) throw error;
+  else if (data.length === 0) {
+    const { data, error } = await supabase
+      .from("bin_info")
+      .insert([
+        {
+          location,
+          description,
+          bintype: JSON.parse(bintype),
+          latitude,
+          longitude,
+          user_update: userId,
+        },
+      ])
+      .select();
     if (error) throw error;
-    else if (data.length === 0 ){
-        const {data, error} = await supabase.from("bin_info").insert([{
-            location,
-            description,
-            bintype,
-            latitude,
-            longitude,
-            user_update:userId
-        }]).select();
-        if (error) throw error;
-        else {
-            res.send(data);
-        }
-    }
     else {
-        res.send("bin already exist");
+      res.send(data);
     }
+  } else {
+    res.send("bin already exist");
+  }
 };
 
 export const addpictureController = async (req, res) => {
-    const file = req.file;
-    const binId = req.body.id;
-    const newminetype = "image/jpeg";
-    const newfilename = `bin_${binId}.jpeg`;
-    let {data, error} = await supabase.from("bin_info").select("*").eq("id", binId);
-    if (error) throw error;
-    else if (data[0].picture === null) {
-        const {data: datapicture, err} = await supabase.storage.from("bin").upload(newfilename, file.buffer, {
-            contentType: newminetype
-        });
-        if (err) throw err;
-        else {
-            const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
-            const {
-                data,
-                err
-            } = await supabase.from("bin_info").update({picture: url}).eq("id", binId).select();
-            if (err) throw err;
-            else {
-                res.send(data);
-            }
-        }
-    } else {
-        const {data, err} = await supabase.storage.from("bin").update(newfilename, file.buffer, {
-            contentType: newminetype
-        });
-        if (err) throw err;
-        else {
-            res.send(data)
-        }
+  const file = req.file;
+  const binId = req.body.id;
+  const newminetype = "image/jpeg";
+  const newfilename = `bin_${binId}.jpeg`;
+  let { data, error } = await supabase
+    .from("bin_info")
+    .select("*")
+    .eq("id", binId);
+  if (error) throw error;
+  else if (data[0].picture === null) {
+    const { data: datapicture, err } = await supabase.storage
+      .from("bin")
+      .upload(newfilename, file.buffer, {
+        contentType: newminetype,
+      });
+    if (err) throw err;
+    else {
+      const url = `https://pyygounrrwlsziojzlmu.supabase.co/storage/v1/object/public/${datapicture.fullPath}`;
+      const { data, err } = await supabase
+        .from("bin_info")
+        .update({ picture: url })
+        .eq("id", binId)
+        .select();
+      if (err) throw err;
+      else {
+        res.send(data);
+      }
     }
-}
+  } else {
+    const { data, err } = await supabase.storage
+      .from("bin")
+      .update(newfilename, file.buffer, {
+        contentType: newminetype,
+      });
+    if (err) throw err;
+    else {
+      res.send(data);
+    }
+  }
+};
