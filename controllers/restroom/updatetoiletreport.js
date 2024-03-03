0fb26e73f64c946fb4fcabecff5e877359b8ad99
I2eNamE <tapanawat2004@gmail.com>
2024-03-03 10:46:40 +0700

    add admin response report again

--- controllers/restroom/updatetoiletreport.js
+++ controllers/restroom/updatetoiletreport.js
@@ -1,7 +1,13 @@
 import supabase from "../database/database.js";
 
 export const updatetoiletreport = async (req, res) => {
-    res.send("updatetoiletreport");
+    const {reportID, status} = req.body;
+    console.log(reportID, status, textaccept(status));
+    const {data, error} = await supabase.from("toilet_report" ).update({status:textaccept(status)}).eq("id",reportID);
+    if (error) {res.status(500).send(error)}
+    else {
+        res.send(data);
+    }
 };
 
 
