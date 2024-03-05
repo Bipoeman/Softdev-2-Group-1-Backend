e612c64561e2b596861eddece57322b9455c0e85
winth03 <66255462+winth03@users.noreply.github.com>
2024-03-05 18:09:16 +0700

    change to allow all image types

--- controllers/restroom/uploadtoiletcomment.js
+++ controllers/restroom/uploadtoiletcomment.js
@@ -2,12 +2,6 @@ import supabase from "../database/database.js";
 
 export const uploadtoiletcomment = async (req, res) => {
   const file = req.file;
-
-  const allowedFileTypes = ["image/png", "image/jpeg"];
-  if (!allowedFileTypes.includes(file.mimetype)) {
-    return res.status(400).send("Invalid file type");
-  }
-
   const { id } = req.body;
   const newmimetype = file.mimetype;
   const newfilename = `toilet_comment_${id}`;
